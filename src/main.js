18dec471248214852b954321c5241cee89c0995d
unknown <mathias.greenhalgh@gmail.com>
2022-05-12 16:01:17 +0200

    controls on

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
+
 import * as THREE from "../node_modules/three/build/three.module.js"
 
-// import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
+// import { OrbitControls } from "./OrbitControls.js";
+import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/controls/OrbitControls.js';
+
+
 
 const bgColour = {
     color: '#ffffff'
@@ -16,13 +20,22 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.SphereGeometry();
+const geometry = new THREE.SphereGeometry(1 , 64, 64);
 const material = new THREE.MeshBasicMaterial( { map: texture } );
 
 const sphere = new THREE.Mesh( geometry, material );
 scene.add( sphere );
 
 camera.position.z = 5;
+
+// let canvas = document.getElementsByTagName('canvas')
+
+// const controls = new OrbitControls(camera, canvas);
+// controls.target.set(0, 5, 0);
+// controls.update();
+
+const controls = new OrbitControls( camera, renderer.domElement );
+
 
 function animate() {
     requestAnimationFrame( animate );
